36e2eb31eb2d15ec3199c8f8d8ad4598eda0d6e3
Geonwoo Pack <packdev937@gmail.com>
2024-10-16 14:20:46 +0900

    feat : 확장성 가능한 Icon으로 재설계한다

--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import {StyleProp, ViewStyle} from 'react-native';
 import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+export type IconLibrary = 'Ionicons' | 'MaterialIcons' | 'FontAwesome' | 'AntDesign';
 
 export interface IconProps {
-  name: keyof typeof Ionicons.glyphMap;
+  library: IconLibrary;
+  name: string;
   color?: string;
   size?: number;
   style?: StyleProp<ViewStyle>;
 }
 
-const Icon = ({name, color, size = 24, style}: IconProps,) => {
-  const fontSize = 24;
-  return (
-    <>
-      {name && (
-        <Ionicons name={name} size={size || fontSize} color={color} style={style}/>
-      )}
-    </>
-  )
-}
+const Icon = ({library, name, color, size = 24, style}: IconProps) => {
+  const renderIcon = () => {
+    switch (library) {
+      case 'Ionicons':
+        return <Ionicons name={name as keyof typeof Ionicons.glyphMap} size={size} color={color} style={style}/>;
+      case 'MaterialIcons':
+        return <MaterialIcons name={name as keyof typeof MaterialIcons.glyphMap} size={size} color={color} style={style}/>;
+      case 'FontAwesome':
+        return <FontAwesome name={name as keyof typeof FontAwesome.glyphMap} size={size} color={color} style={style}/>;
+      case 'AntDesign':
+        return <AntDesign name={name as keyof typeof AntDesign.glyphMap} size={size} color={color} style={style}/>;
+      default:
+        return null;
+    }
+  };
 
-export default Icon
+  return <>{renderIcon()}</>;
+};
+
+export default Icon;
